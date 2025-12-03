4a63ff1d4e4628fddf54202c363c3d635f9e4742
Amsh23 <amsh23@gmail.com>
2025-12-03 14:25:18 +0330

    Add Gmail SMTP utility and update README
    Added a new gmailsmtp module with environment example, requirements, .gitignore, and a Python script for sending emails with attachments using yagmail and dotenv. The README was expanded with detailed project information, features, usage instructions, and file structure.

---
+++ gmailsmtp/smtpgmail.py
@@ -0,0 +1,36 @@
+#ph.rafie@gmail.com
+from dotenv import load_dotenv
+import os
+import yagmail
+
+load_dotenv()
+
+sender = os.getenv("EMAIL_ADDRESS")
+app_password = os.getenv("EMAIL_APP_PASSWORD")
+
+receiver = "am.shiii2003@gmail.com"
+
+
+attachment_path = os.path.join(os.path.dirname(__file__), "project.zip")
+
+
+if not os.path.exists(attachment_path):
+    print(f"خطا: فایل {attachment_path} یافت نشد!")
+    exit(1)
+
+yag = yagmail.SMTP(sender, app_password)
+
+yag.send(
+    to=receiver,
+    subject="پروژه نهایی پایتون مجتمع فنی امیر شیرخدائی طاری",
+    contents="""
+سلام استاد،
+پروژه کامل در فایل ZIP پیوست شده.
+لینک گیت‌هاب پروژه:
+https://github.com/Amsh23/animepuzzleMCV-mt-mp
+با تشکر
+""",
+    attachments=attachment_path
+)
+
+print("ایمیل ارسال شد!")
