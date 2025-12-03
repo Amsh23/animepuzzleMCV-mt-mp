21e2d6fdb07bc7069073d385af3ceff1481364c6
Amsh23 <amsh23@gmail.com>
2025-12-03 15:05:44 +0330

    Update smtpgmail.py

--- gmailsmtp/smtpgmail.py
+++ gmailsmtp/smtpgmail.py
@@ -1,4 +1,4 @@
-#ph.rafie@gmail.com
+# ph.rafie@gmail.com
 from dotenv import load_dotenv
 import os
 import yagmail
@@ -10,27 +10,25 @@ app_password = os.getenv("EMAIL_APP_PASSWORD")
 
 receiver = "am.shiii2003@gmail.com"
 
-
-attachment_path = os.path.join(os.path.dirname(__file__), "project.rar")
-
-
-if not os.path.exists(attachment_path):
-    print(f"خطا: فایل {attachment_path} یافت نشد!")
-    exit(1)
-
+# SMTP اتصال با جیمیل
 yag = yagmail.SMTP(sender, app_password)
 
 yag.send(
     to=receiver,
-    subject="پروژه نهایی پایتون مجتمع فنی امیر شیرخدائی طاری",
-    contents="""
-سلام استاد،
-پروژه کامل در فایل ZIP پیوست شده.
+    subject="پروژه نهایی پایتون - مجتمع فنی امیر شیرخدائی طاری",
+    contents=f"""
+سلام استاد وقت بخیر،
+به دلیل حجم بالای فایل پروژه، نسخه کامل داخل Google Drive قرار داده شده و از لینک زیر قابل دانلود است:
+
+Google Drive Link:
+https://drive.google.com/file/d/1ym2yypXj3cfUBEnrxPODttKN1BJXWoYY/view?usp=drive_link
+
 لینک گیت‌هاب پروژه:
 https://github.com/Amsh23/animepuzzleMCV-mt-mp
-با تشکر
-""",
-    attachments=attachment_path
+
+با تشکر و احترام
+امیر شیرخدائی طاری
+"""
 )
 
 print("ایمیل ارسال شد!")
